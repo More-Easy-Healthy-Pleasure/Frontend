1d1eb9a2692864501b5d8d75b354a1a529a14873
ssook <60427387+ssook1222@users.noreply.github.com>
2024-04-18 04:49:41 +0000

    UPDATE : 컨테이너 안에 버튼 배치

--- frontend/src/view/Main/Main.jsx
+++ frontend/src/view/Main/Main.jsx
@@ -1,26 +1,35 @@
 import React from "react";
+import { Button, Col, Row } from "react-bootstrap";
 import './Main.css';
 
 function Main() {
     return (
         <div className="main-container">
-            <div className="container green">
-                <div className="fade-in-out">
-                    더 쉽고,
-                </div>
-            </div>
-            <div className="container white">
+            <Row className="container green">
+                <Row>
+                    <div className="fade-in-out">
+                        더 쉽고,
+                    </div>
+                </Row>
+                <Row className="green">
+                    <Button variant="outline-light">레시피 추천받기</Button>
+                </Row>
+            </Row>
+            <Row className="container white">
                 <div className="fade-in-out">더 싸게,</div>
-            </div>
-            <div className="container split">
-                <div className="col green-text">당신의 헬시플레</div>
-                <div className="col white-text">저를 지키세요.</div>
-            </div>
-            <div className="container">
-                <div className="container white">
+                <Row className="white">
+                    <Button variant="outline-dark">시세 확인하기</Button>
+                </Row>
+            </Row>
+            <Row className="container split">
+                <Col className="green-text">당신의 헬시플레</Col>
+                <Col className="white-text">저를 지키세요.</Col>
+            </Row>
+            <Row className="container">
+                <Row className="container white">
                     <div className="fade-in-out">건강을 담다, 건담.</div>
-                </div>
-            </div>
+                </Row>
+            </Row>
         </div>
     );
 }
