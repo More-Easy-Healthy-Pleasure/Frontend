d64e98fc6fc7f237d170af99e5f5235516e024e3
ssook <60427387+ssook1222@users.noreply.github.com>
2024-04-20 16:50:03 +0900

    [Feature/main] UPDATE : 간격 조정 (#5)
    * UPDATE : 메인페이지 틀만 생성
    
    * UPDATE : 페이드 인-아웃 영역 추가작업
    
    * UPDATE : 컨테이너 안에 버튼 배치
    
    * ADD : 네비게이션 바 추가
    
    * ADD : 메인 페이지 업데이트

--- frontend/src/view/Main/Main.jsx
+++ frontend/src/view/Main/Main.jsx
@@ -7,13 +7,13 @@ function Main() {
     return (
         <>
             <div className="main-top-container">
-                <Row className="main-container green">
+                <Row className="main-container green" style={{height: "33vh"}}>
                     <Row>
-                        <div className="fade-in-out" style={{ marginBottom: "-10%" }}>
+                        <div className="fade-in-out" >
                             더 쉽고,
                         </div>
                     </Row>
-                    <Row className="green" style={{ marginTop: "-5%" }}>
+                    <Row className="green">
                         <Button
                             variant="outline-light"
                             className="fade-in-out"
@@ -22,8 +22,8 @@ function Main() {
                         </Button>
                     </Row>
                 </Row>
-                <Row className="main-container white">
-                    <div className="fade-in-out" style={{ marginBottom: "-10%" }}>더 싸게,</div>
+                <Row className="main-container white" style={{height: "33vh"}}>
+                    <div className="fade-in-out"> 더 싸게,</div>
                     <Row className="white">
                         <Button
                             variant="outline-dark"
@@ -33,17 +33,17 @@ function Main() {
                         </Button>
                     </Row>
                 </Row>
-                <Row className="main-container split">
+                <Row className="main-container split" style={{height: "33vh"}}>
                     <Col className="green-text">당신의 헬시플레</Col>
                     <Col className="white-text">저를 지키세요.</Col>
                 </Row>
-                <Row className="main-container">
+                <Row className="main-container" style={{marginTop:"-1%"}}>
                     <Row 
                         className="nav-container fade-in-out"
                         style={{ marginTop: "-5%", width:"100%" }}>
                         <Navibar></Navibar>
                     </Row>
-                    <Row className="main-container white">
+                    <Row className="main-container white" style={{ height: "45vh"}}>
                         <div
                             className="fade-in-out"
                             style={{ fontSize: "2rem" }}
