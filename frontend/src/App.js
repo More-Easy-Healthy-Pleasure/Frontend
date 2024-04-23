d1dc653a9bd71a281fd04030b9505cce1f8a057a
ssook <60427387+ssook1222@users.noreply.github.com>
2024-04-23 08:23:21 +0000

    ADD : 레시피 메인 페이지 틀 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Main from './view/Main/Main.jsx'
+import Recipe from './view/Recipe/Recipe.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Main />} />
+        <Route exact path="/Recipe" element={<Recipe />} />
       </Routes>
     </BrowserRouter>
   );
