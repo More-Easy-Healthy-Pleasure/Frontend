f604deb149d210aaaa9330db095adfa0b3abed33
ssook <60427387+ssook1222@users.noreply.github.com>
2024-04-26 16:01:22 +0900

    [Feature/recipe] UPDATE : 검색 바 생성 (#8)
    * UPDATE : 메인페이지 틀만 생성
    
    * UPDATE : 페이드 인-아웃 영역 추가작업
    
    * UPDATE : 컨테이너 안에 버튼 배치
    
    * ADD : 네비게이션 바 추가
    
    * ADD : 메인 페이지 업데이트
    
    * UPDATE : 메인페이지 1차 세팅 완료
    
    * ADD : 레시피 메인 페이지 틀 생성
    
    * UPDATE : 검색 바 생성

--- frontend/src/view/Recipe/Recipe.jsx
+++ frontend/src/view/Recipe/Recipe.jsx
@@ -4,11 +4,13 @@ import './Recipe.css';
 
 import Navibar from "../../Component/Navibar";
 import Footer from "../../Component/Footer";
+import RecipeContainer from "./RecipeContainer";
 
 function Recipe() {
     return (
         <>
             <Navibar></Navibar>
+            <RecipeContainer></RecipeContainer>
             <Footer></Footer>
         </>
 
